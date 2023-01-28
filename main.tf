7d8827f6ac22eb52846df1f7c809da0625ba7200
thiyagarajanshankar13 <thiyagarajan.shankar13@gmail.com>
2023-01-29 10:59:26 +1100

    bucket template code

--- main.tf
+++ main.tf
@@ -2,3 +2,21 @@ resource "google_storage_bucket" "bucket" {
   name     = "test-bucket-rohit"
   location = "australia-southeast1"
 }
+
+locals {
+  bucketslist = yamldecode(file("./configs/gcs-buckets/bucket-details.yaml"))["buckets"]
+  bucketitems = flatten([
+    for bucket in local.bucketslist : {
+      bucketname = bucket.bucket_name
+      bucketlocation = bucket.location
+    }
+  ])
+}
+
+resource "google_storage_bucket" "bucket" {
+  for_each = {
+    for bucket in local.bucketitems : "${bucket.bucketname}/${bucket.bucketlocation}" => bucket
+  }
+  name     = "${each.value.bucketname}"
+  location = "${each.value.bucketlocation}"
+}
